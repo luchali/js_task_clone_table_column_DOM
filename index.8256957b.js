cf1d86a30ae5b72ac3baf7f22861800fffa77a1f
Alina Luchytska <aluchitska@gmail.com>
2024-10-23 12:21:56 +0300

    make build

---
+++ index.8256957b.js
@@ -0,0 +1,2 @@
+document.querySelector("table").querySelectorAll("tr").forEach(function(e){var r=e.querySelectorAll("td, th");if(r.length>1){var t=r[1].cloneNode(!0);e.insertBefore(t,r[r.length-1])}});
+//# sourceMappingURL=index.8256957b.js.map
